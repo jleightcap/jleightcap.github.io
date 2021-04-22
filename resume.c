62403940c2cf3132f9025f6f8077a666e311a1a4
jleightcap <jleightcap@protonmail.com>
2021-04-22 02:36:47 -0400

    update resume

--- resume.c
+++ resume.c
@@ -6,7 +6,7 @@
 /* for a human-readable resume, email me        */
 /*==============================================*/
 /* do you enjoy this monstrosity? */ //??/
-register do { //
+extern "lisp" { //
 #include<stdio.h>//" I@BJ KDHFGSB@O	ikdhfgsb`o?oq
 int main(int c,char**v){//nsnml`hk-bnl ,, fhsgta-
 char b['Z'];//bnl.ikdhfgsb`o ,, i`bj-kdhfgsb`o-bn
@@ -24,47 +24,49 @@ case 34://qhcfd Tmhudqrhsx+ Rtlldq 1/08	Odlaqnjd,
 ++i;break;//r Rbgnk`qrgho+ Mnqsgd`rsdqm FDN Fq`ms
 case 0x23://	"" Hmsdqdrsr	Rxrsdlr Rnesv`qd+ Bnlot
 !(b[i&~0xc000]-32)?//sdq @qbghsdbstqd+ Nodq`shmf 
-printf(" ")://Rxrsdlr+ Bnlohkdqr	" RJHKKR	B+ Oxsg
-printf("%c",-??-(~(~i|49152))[b]);//nm+ Qtrs+ B**
-i++;break;//+ @rrdlakx 'w75+ @QL+ @UQ+ QHRB,U(+ R
-default: (*(signed*)0x0D15EA5E)++;//gdkk+ Udqhknf
-}??>}}//+ NB`lk+ Q`bjds	" DWODQHDMBD	"" Qdrd`qbg 
-//Dmfhmddq Bn,No9 Rbhdmshehb Rxrsdlr 'Itkx 1/1/, 
-//Cdbdladq 1/1/(	, sq`mrhshnm oqnidbs sn mdv dlad
-//ccdc `qbghsdbstqd vhsg Xnbsn Khmtw	, hlokdldms 
-//rdqh`k bnlltmhb`shnm oqnsnbnk eqnl fhud rodbheh
-//b`shnm hm Qtrs	, cdrhfm `mnl`kx cdsdbshnm rxrsd
-//lr enq cdsdqlhmhmf bnqqdk`shnmr hm cxm`lhbr c`s
-//`	"" Qdrd`qbg @rrhrs`ms9 MTB@Q 'Roqhmf 1/1/ , R
-//oqhmf 1/1/(	, qdrd`qbg `rrhrs`ms hm Mnqsgd`rsdq
-//m Tmhudqrhsx Bnlotsdq @qbghsdbstqd Qdrd`qbg k`a
-//	, vnqj nm QHRB,U hmrsqtbshnm rds rhltk`snq	"" 
-//Uhbd Oqdrhcdms+ Vnqjrgno Bnnqchm`snq9 Vhqdkdrr 
-//Bkta+ HDD 'E`kk 1/08 , Oqdrdms(	, Cdrhfm `mc qt
-//m hmsdq`bshud vnqjrgnor enq Khmtw+ K`SdW+ Fhs+ 
-//B**+ `mc UHL	, Fhud s`kj nm dladccdc rxrsdlr cd
-//udknoldms dwoknqhmf kdudkr ne `arsq`bshnm hm	  
-//vqhshmf ` _akhmj& oqnfq`l-	  Bnlo`qd hlokdldms`
-//shnmr hm B+ Qtrs+ `mc g`mc,vqhssdm @UQ @rrdlakx
-//-	  Hmbktcdc chrbtrrhnmr ne rxrsdlr rnesv`qd+ b
-//nlotsdq `qbghsdbstqd+ ldlnqx	  gdhq`qbgx+ `mc a
-//thkc rxrsdlrf	, Fdmdq`k bk`rr `l`sdtq q`chn khb
-//dmrd+ kd`c g`l q`chn rstcx rdrrhnmr `mc	  lddsh
-//mfr	" OQNIDBSR	"" I@K Oqnfq`llhmf K`mft`fd	, Vq
-//nsd bnlohkdq hm B eqnl rbq`sbg s`qfdshmf Y7/ `r
-//rldakx enq nvm oqnfq`llhmf	  k`mft`fd	, Cdrhfmd
-//c oqnfq`llhmf k`mft`fd sg`s rddjr sn bnlahmd sg
-//d hldq`shud+	  rxrsdlr,nqhdmsdc ed`stqdr ne B v
-//hsg ` etmbshnm`k rxms`w	"" Idhfgsx Lhbqnbnlotsd
-//q	, Cdrhfmdc `mc athks Y7/,a`rdc aqd`can`qc lhm
-//hbnlotsdq	, Vqnsd lhbqnjdqmdk hm I@K `mc Y7/ `r
-//rdlakx+ hmbktchmf g`qcv`qd cqhudqr `mc 	  rs`mc
-//`qc khaq`qx	"" gl`kknb	, Vqnsd ` sgqd`c,r`ed at
-//bjds,a`rdc ldlnqx `kknb`snq hm B+ cdrhfmdc sn a
-//d e`rsdq	  hm bnllnm trd sg`m sgd fkhab l`kknb	
-//, Cdrhfm hmbktcdc noshlhyhmf c`s` rsqtbstqdr+ s
-//gqd`c,r`edsx ldbg`mhrlr+ `mc	  `rxlosnshb bnlok
-//dwhsx	"" Bhltkhmj	, Annkd`m @kfdaq` dwoqdrrhnm 
-//qdctbdq vqhssdm hm B+ o`qrd dwoqdrrhnmr `mc qdc
-//tbd	  fhudm ` rds ne `whnlr	, Hlokdldmsdc du`kt
-//`shnm nm Whkhmw Yxmp,6/// RnB vhsg rxrsdl H.N	
+printf(" ")://Rxrsdlr+ Bnlohkdqr	" RJHKKR	B+ Qtrs
+printf("%c",-??-(~(~i|49152))[b]);//+ B**+ @rrdla
+i++;break;//kx 'w75+ @QL+ @UQ+ QHRB,U(+ Udqhknf	O
+default: (*(signed*)0x0D15EA5E)++;//xsgnm+ IR.Sxo
+}??>}}//dRbqhos+ Rgdkk+ NB`lk+ Q`bjds	" DWODQHDMB
+//D	"" Qdrd`qbg Dmfhmddq Bn,No9 Rbhdmshehb Rxrsdl
+//r 'Itkx 1/1/ , Cdbdladq 1/1/(	, Sq`mrhshnm oqni
+//dbs sn mdv dladccdc `qbghsdbstqd vhsg Xnbsn Khm
+//tw	, Hlokdldms rdqh`k bnlltmhb`shnm oqnsnbnk eq
+//nl fhud rodbhehb`shnm hm Qtrs	, Cdrhfm `mnl`kx 
+//cdsdbshnm rxrsdlr enq cdsdqlhmhmf bnqqdk`shnmr 
+//hm cxm`lhbr c`s`	"" Qdrd`qbg @rrhrs`ms9 MTB@Q '
+//Roqhmf 1/08 , Roqhmf 1/1/(	, Qdrd`qbg `rrhrs`ms
+// hm Mnqsgd`rsdqm Tmhudqrhsx Bnlotsdq @qbghsdbst
+//qd Qdrd`qbg k`a	, Bnmsqhatsd sn QHRB,U hmrsqtbs
+//hnm rds rhltk`snq	"" Uhbd Oqdrhcdms+ Vnqjrgno B
+//nnqchm`snq9 Vhqdkdrr Bkta+ HDDD 'E`kk 1/08 , Oq
+//drdms(	, Cdrhfmdc `mc q`m hmsdq`bshud vnqjrgnor
+// bnudqhmf Khmtw+ K`SdW+ Fhs+ B**+ UHL+	  dladcc
+//dc rxrsdlr oqnfq`llhmf+ rnkcdqhmf+ `mc OBA cdrh
+//fm	, Fhud s`kj nm dladccdc rxrsdlr cdudknoldms 
+//dwoknqhmf kdudkr ne `arsq`bshnm hm	  vqhshmf ` 
+//&akhmj& oqnfq`l-	  Bnlo`qd hlokdldms`shnmr hm B
+//+ Qtrs+ `mc g`mc,vqhssdm @UQ @rrdlakx-	  Hmbktc
+//dc chrbtrrhnmr ne rxrsdlr rnesv`qd+ bnlotsdq `q
+//bghsdbstqd+ ldlnqx	  gdhq`qbgx+ `mc athkc rxrsd
+//lr	, Fdmdq`k bk`rr `l`sdtq q`chn khbdmrd+ kd`c 
+//rstcx rdrrhnmr `mc lddshmfr	" OQNIDBSR	"" I@K O
+//qnfq`llhmf K`mft`fd	, Vqnsd bnlohkdq hm B eqnl 
+//rbq`sbg s`qfdshmf Y7/ `rrdlakx enq nvm oqnfq`ll
+//hmf	  k`mft`fd	, Cdrhfmdc oqnfq`llhmf k`mft`fd 
+//sg`s rddjr sn bnlahmd sgd hldq`shud+	  rxrsdlr,
+//nqhdmsdc ed`stqdr ne B vhsg ` etmbshnm`k rxms`w
+//	"" Idhfgsx Lhbqnbnlotsdq	, Cdrhfmdc `mc athks 
+//Y7/,a`rdc aqd`can`qc lhmhbnlotsdq	, Vqnsd lhbqn
+//jdqmdk hm I@K `mc Y7/ `rrdlakx+ hmbktchmf g`qcv
+//`qd cqhudqr `mc	  rs`mc`qc khaq`qx	"" gl`kknb	,
+// Vqnsd ` sgqd`c,r`ed atbjds,a`rdc ldlnqx `kknb`
+//snq hm B+ cdrhfmdc sn ad e`rsdq	  hm bnllnm trd
+// sg`m sgd fkhab l`kknb	, Cdrhfm hmbktcdc noshlh
+//yhmf c`s` rsqtbstqdr+ sgqd`c,r`edsx ldbg`mhrlr+
+// `mc	  `rxlosnshb bnlokdwhsx	"" Bhltkhmj	, Annk
+//d`m @kfdaq` dwoqdrrhnm qdctbdq vqhssdm hm B+ o`
+//qrd dwoqdrrhnmr `mc qdctbd	  fhudm ` rds ne `wh
+//nlr	, Hlokdldmsdc du`kt`shnm nm Whkhmw Yxmp,6//
+/// RnB vhsg rxrsdl H.N	
